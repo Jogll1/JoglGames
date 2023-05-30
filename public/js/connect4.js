024bccb5149760445e4528f91f5b90f6c795d315
JoelLuckett <joelluckett@icloud.com>
2023-05-30 13:27:47 +0100

    Added square tiles to board

--- public/js/connect4.js
+++ public/js/connect4.js
@@ -2,7 +2,9 @@ const rows = 6;
 const columns = 7;
 
 // When document loads up call setGame()
-$(document).ready(setGame());
+$(document).ready(function() {
+    setGame();
+});
 
 function setGame() {
     //board will be a 2d array
@@ -16,6 +18,20 @@ function setGame() {
         //columns
         for (let c = 0; c < columns; c++) {
             row.push(' '); //add an empty value to each column of a row
+
+            //#region Creating tiles
+            //creates <div id="0-0" class="tile"></div> in the board div
+            //The square background has id SQ0-0, the cicular tile has 0-0
+            //A square shaped div is created behind the circular tile to detect the hover
+            let idNo = r.toString() + "-" + c.toString();
+            let squareTile = document.createElement("div");
+            squareTile.id = "SQ" + idNo;
+            squareTile.classList.add("squareTile");
+
+            //append squareTile to board
+            $('#board').append(squareTile);
+
+            //#endregion
         }
         board.push(row);
     }
