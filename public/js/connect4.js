9a32f064843e05b0e519f495c5306439a36169fe
JoelLuckett <joelluckett@icloud.com>
2023-06-06 22:29:23 +0100

    Made columns highlight when hovered

--- public/js/connect4.js
+++ public/js/connect4.js
@@ -1,5 +1,6 @@
 const rows = 6;
 const columns = 7;
+//const currentColumns = [5, 5, 5, 5, 5, 5, 5];
 
 //test for using global modules
 var c4_module = (function(){ //create a module
@@ -15,8 +16,6 @@ var c4_module = (function(){ //create a module
         }
     }
 })();
-
-
 
 // When document loads up call setGame()
 $(document).ready(function() {
@@ -40,10 +39,29 @@ $(document).ready(function() {
         $('.onlinePlayMenu').hide();
     });
 
-    $('.squareTile').click(function(){
+    //change the colour of the tile when its clicked on
+    $('.squareTile').click(function() {
         let id = $(this).attr("id"); //get the squaretile's id
         let coords = id.substring(2); //remove the SQ from the front
         setPiece(coords);
+    });
+
+    //when hovering over a column
+    $('.squareTile').mouseenter(function() {
+        let id = $(this).attr("id"); //get the squaretile's id
+        let columnNo = id.substring(2).split("-")[1]; //get the column number of the tile
+
+        //select the correct column hover to show
+        $('#Hover' + columnNo).addClass("hoverSelected");
+    });
+
+    //when mouse leaves a tile
+    $('.squareTile').mouseleave(function() {
+        let id = $(this).attr("id"); //get the squaretile's id
+        let columnNo = id.substring(2).split("-")[1]; //get the column number of the tile
+
+        //select the correct column hover to hide
+        $('#Hover' + columnNo).removeClass("hoverSelected");
     });
 });
 
@@ -85,7 +103,34 @@ function setGame() {
         board.push(row);
     }
 
+    setBoardHovers();
     logArray(board);
+}
+
+//function to create the column hover divs over the board
+function setBoardHovers() {
+    //left hover
+    let leftBoardHoverDiv = document.createElement("div"); //create a new div
+    leftBoardHoverDiv.id = "Hover0"; //give it the correct indexed id
+    leftBoardHoverDiv.classList.add("boardColumnHoverLeft"); //add the class to style it
+
+    $('#boardColumnHoversParent').append(leftBoardHoverDiv); //make the boardColumnHoversParent this div's parent
+
+    //centre hovers
+    for (let i = 0; i < columns - 2; i++) {
+        let boardHoverDiv = document.createElement("div"); //create a new div
+        boardHoverDiv.id = "Hover" + (i + 1); //give it the correct indexed id
+        boardHoverDiv.classList.add("boardColumnHover"); //add the class to style it
+
+        $('#boardColumnHoversParent').append(boardHoverDiv); //make the boardColumnHoversParent this div's parent
+    }
+
+    //right hover
+    let rightBoardHoverDiv = document.createElement("div"); //create a new div
+    rightBoardHoverDiv.id = "Hover6"; //give it the correct indexed id
+    rightBoardHoverDiv.classList.add("boardColumnHoverRight"); //add the class to style it
+
+    $('#boardColumnHoversParent').append(rightBoardHoverDiv); //make the boardColumnHoversParent this div's parent
 }
 
 function setPiece(id) {
@@ -93,7 +138,7 @@ function setPiece(id) {
 
     console.log("clicked at " + id);
 
-    $('#' + id).addClass("selected");
+    $('#' + id).addClass("yellowTile");
 }
 
 // function that console.logs the values in a 2d array (or normal array)
