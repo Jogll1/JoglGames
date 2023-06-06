de8b7aeaba38dc932f58b9394730c4ecd6a91fe7
JoelLuckett <joelluckett@icloud.com>
2023-06-06 09:40:19 +0100

    added a module to handle globals and made tiles turn white when clicked if playing robot

--- public/js/connect4.js
+++ public/js/connect4.js
@@ -1,6 +1,23 @@
 const rows = 6;
 const columns = 7;
 
+//test for using global modules
+var c4_module = (function(){ //create a module
+    var gameStarted = false; //create a variable inside the module (within scope)
+
+    return { //return a fuction that sets the variable
+        setGameStarted : function(bToSet) {
+            return gameStarted = bToSet;
+        },
+
+        getGameStarted : function() {
+            return gameStarted;
+        }
+    }
+})();
+
+
+
 // When document loads up call setGame()
 $(document).ready(function() {
     setGame();
@@ -8,6 +25,9 @@ $(document).ready(function() {
     $('#playRobotButton').click(function() {
         $('.menuBackground').hide();
         $('.friendOrAIMenu').hide();
+
+        c4_module.setGameStarted(true);
+        // console.log(c4_module.getGameStarted());
     });
 
     $('#playFriendButton').click(function() {
@@ -18,6 +38,12 @@ $(document).ready(function() {
     $('#playOnlineButton').click(function() {
         $('.menuBackground').hide();
         $('.onlinePlayMenu').hide();
+    });
+
+    $('.squareTile').click(function(){
+        let id = $(this).attr("id"); //get the squaretile's id
+        let coords = id.substring(2); //remove the SQ from the front
+        setPiece(coords);
     });
 });
 
@@ -53,13 +79,20 @@ function setGame() {
 
             //append circular tile to squareTile
             $('#SQ' + idNo).append(circularTile);
-
             //#endregion
         }
         board.push(row);
     }
 
     logArray(board);
+}
+
+function setPiece(id) {
+    if(!c4_module.getGameStarted()) return;
+
+    console.log("clicked at " + id);
+
+    $('#' + id).addClass("selected");
 }
 
 // function that console.logs the values in a 2d array (or normal array)
