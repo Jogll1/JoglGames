c20523ffca752c279ef89e074f934662e319f985
JoelLuckett <joelluckett@icloud.com>
2023-06-08 15:11:27 +0100

    something

--- public/js/connect4AI.js
+++ public/js/connect4AI.js
@@ -1,46 +1,62 @@
 function minimaxab(board, depth, a, b, maximisingPlayer) { //minimax with alpha beta pruning
-    //getValidPositions() //get valid positions of the board function
+    //validPositions = getValidPositions(board) //get valid positions of the board function
     //isTerminal = isTerminalNode(board); //check to see if this node is the terminal position
     if(depth == 0) { //or isTerminal
         //return heuristic value of teminal node
         //if(isTerminal) {
         //    if(isWinningMove(board, AIPiece)) {
-        //        return (None, 100000000000000);
+        //        return (null, 100000000000000);
         //    }
         //    else if(isWinningMove(board, PlayerPiece)) {
-        //        return (None, -100000000000000);
+        //        return (null, -100000000000000);
         //    }
         //    else { //game is over, no valid moves
-        //        return (None, 0);
+        //        return (null, 0);
         //    } 
         //}
         //else { //depth is 0
-        //    return (None, scorePosition(board, AIPiece))
+        //    return (null, scorePosition(board, AIPiece))
         //}
     }
 
     if(maximisingPlayer) { //maximising player
         let value = -Infinity;
+        //column = board[Math.floor(Math.random() * array.length)];
 
-        for (let i = 0; i < node.length; i++) {
-            value = Math.max(value, minimaxab(node[i], (depth - 1), a, b, false)); //start the recursion
-            a = Math.max(a, value);
-            if(a >= b) {
-                break; //b cut off
-            }
-        }
-        return value;
+        //for (let i = 0; i < validPositions.length; i++) {
+        //    let row = getNextOpenRow(board);
+        //    let boardCopy = board;
+        //    setPiece(columnNo, AIPiece);
+        //    let newScore = minimaxab(boardCopy, (depth - 1), a, b, false)[1]
+        //    if(newScore > value) {
+        //        value = newScore
+        //        column = board[i];
+        //    }
+        //    a = max(a, value);
+        //    if(a >= b) {
+        //        break;
+        //    }
+        //}
+        //return (column, value);
     }
     else { //minimising player
         let value = Infinity;
+        //column = board[Math.floor(Math.random() * array.length)];
 
-        for (let i = 0; i < node.length; i++) {
-            value = Math.min(value, minimaxab(node[i], (depth - 1), a, b, true)); //start the recursion
-            b = Math.min(b, value);
-            if(b <= a) {
-                break; //alpha cutoff
-            }
-        }
-        return value;
+        //for (let i = 0; i < validPositions.length; i++) {
+        //    let row = getNextOpenRow(board);
+        //    let boardCopy = board;
+        //    setPiece(columnNo, playerPiece);
+        //    let newScore = minimaxab(boardCopy, (depth - 1), a, b, true)[1]
+        //    if(newScore < value) {
+        //        value = newScore
+        //        column = board[i];
+        //    }
+        //    b = min(b, value);
+        //    if(a >= b) {
+        //        break;
+        //    }
+        //}
+        //return (column, value);
     }
 }
