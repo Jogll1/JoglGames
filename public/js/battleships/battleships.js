9cff0b3a8b0fd062fa3e7d9e59f8b09b7f352246
JoelLuckett <joelluckett@icloud.com>
2023-08-22 20:47:48 +0100

    trying to make it so boats aren't randomly adjacent

--- public/js/battleships/battleships.js
+++ public/js/battleships/battleships.js
@@ -40,10 +40,10 @@ $(document).ready(function() {
     placeBoats()
 
     $(document).on('mousedown', '.gridTile', function() {
-        // $(this).addClass("boatTile");
-        // $(this).addClass("boatRightTile");
-        // console.log($(this).attr("id"));
+        
     });
+
+    logArray(ba_myBoard.getBoard());
 
     // const initScale = 450 / 1920;
     // $(window).resize(function() {
@@ -98,7 +98,7 @@ function setGame() {
 }
 
 //function to place a random boat on the grid
-function placeRandomBoat(length, colour) {
+function placeRandomBoat(_length, _i, _colour) {
     const vertOrHor = getRandomInt(0, 1); //0 = vertical, 1 = horizontal
 
     let ranX = getRandomInt(0, 9);
@@ -106,80 +106,98 @@ function placeRandomBoat(length, colour) {
 
     //make sure boat can fit in grid
     if(vertOrHor === 0) {
-        if(9 - ranX < length) {
+        if(9 - ranX < _length) {
             //if boat won't fit on grid, move it back
-            const extraDist = length - (9 - ranX);
+            const extraDist = _length - (9 - ranX);
             ranX = ranX - extraDist;
         }
     }
     else {
-        if(ranY < length) {
+        if(ranY < _length) {
             //if boat won't fit on grid, move it down
-            const extraDist = length - ranY;
+            const extraDist = _length - ranY;
             ranY = ranY + extraDist;
         }
     }
 
     const gridArray = copy2DArray(ba_myBoard.getBoard());
 
-    //check if boat will not overlap with anything
-    for (let i = 0; i < length; i++) {
-        if(vertOrHor === 0) {
-            if(gridArray[ranX + i][ranY] === 'o') return false;
-
-            //check if adjacent tiles are empty too
-            if(ranY + 1 <= 9) {
-                if(gridArray[ranX + i][ranY + 1] === 'o') return false;
-            }
-
-            if(ranY - 1 >= 0) {
-                if(gridArray[ranX + i][ranY - 1] === 'o') return false;
-            }
-        }
-        else {
-            if(gridArray[ranX][ranY - i] === 'o') return false;
-
-            //check if adjacent tiles are empty too
-            if(ranX + 1 <= 9) {
-                if(gridArray[ranX + 1][ranY - i] === 'o') return false;
-            }
-
-            if(ranX - 1 >= 0) {
-                if(gridArray[ranX - 1][ranY - i] === 'o') return false;
-            }
-        }
-    }
-    
-    //place tiles
-    for (let i = 0; i < length; i++) {
-        if(i === 0) {
-            //update array
-            gridArray[ranX][ranY] = 'o';
-
-            $(`#my${ranX}-${ranY}`).addClass(`boatTile ${vertOrHor === 0 ? "boatTopTile" : "boatRightTile"}`);
-            $(`#my${ranX}-${ranY}`).css("background-color", colour);
-        }
-        else {
-            if(vertOrHor === 0) {
+    if(isValidPlacement(gridArray, vertOrHor, _length, ranX, ranY)) {
+        //place tiles (down and left)
+        for (let i = 0; i < _length; i++) {
+            if(i === 0) {
                 //update array
-                gridArray[ranX + i][ranY] = 'o';
+                gridArray[ranX][ranY] = _i;
+                // logArray(gridArray);
 
-                //horizontal
-                $(`#my${ranX + i}-${ranY}`).addClass(`boatTile ${i < length - 1 ? "boatTile" : "boatBottomTile"}`);
-                $(`#my${ranX + i}-${ranY}`).css("background-color", colour);
+                $(`#my${ranX}-${ranY}`).addClass(`boatTile ${vertOrHor === 0 ? "boatTopTile" : "boatRightTile"}`);
+                $(`#my${ranX}-${ranY}`).css("background-color", _colour);
             }
             else {
-                //update array
-                gridArray[ranX][ranY - i] = 'o';
+                if(vertOrHor === 0) {
+                    //update array
+                    gridArray[ranX + i][ranY] = _i;
+                    // logArray(gridArray);
 
-                //vertical
-                $(`#my${ranX}-${ranY - i}`).addClass(`boatTile ${i < length - 1 ? "boatTile" : "boatLeftTile"}`);
-                $(`#my${ranX}-${ranY - i}`).css("background-color", colour);
+                    //horizontal
+                    $(`#my${ranX + i}-${ranY}`).addClass(`boatTile ${i < _length - 1 ? "boatTile" : "boatBottomTile"}`);
+                    $(`#my${ranX + i}-${ranY}`).css("background-color", _colour);
+                }
+                else {
+                    //update array
+                    gridArray[ranX][ranY - i] = _i;
+                    // logArray(gridArray);
+
+                    //vertical
+                    $(`#my${ranX}-${ranY - i}`).addClass(`boatTile ${i < _length - 1 ? "boatTile" : "boatLeftTile"}`);
+                    $(`#my${ranX}-${ranY - i}`).css("background-color", _colour);
+                }
             }
         }
-    }
 
-    ba_myBoard.updateBoard(gridArray);
+        ba_myBoard.updateBoard(gridArray);
+
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//check for valid boat placement
+function isValidPlacement(_gridArray, _vertOrHor, _length, _ranX, _ranY) {
+    for (let i = 0; i < _length; i++) {
+        if(_vertOrHor === 0) {
+            if(i === 0 && _ranY - 1 >= 0) {
+                if(_gridArray[_ranX][_ranY - 1] !== ' ') return false;
+
+                if(_ranX + 1 <= 9) {
+                    if(_gridArray[_ranX + 1][_ranY - 1] !== ' ') return false;
+                }
+
+                if(_ranX - 1 >= 0) {
+                    if(_gridArray[_ranX - 1][_ranY - 1] !== ' ') return false;
+                }
+            }
+
+            if(i === length - 1 && _ranY + 1 <= 9) {
+                if(_gridArray[_ranX][_ranY + 1] !== ' ') return false;
+
+                if(_ranX + 1 <= 9) {
+                    if(_gridArray[_ranX + 1][_ranY + 1] !== ' ') return false;
+                }
+
+                if(_ranX - 1 >= 0) {
+                    if(_gridArray[_ranX - 1][_ranY + 1] !== ' ') return false;
+                }
+            }
+
+            if(_gridArray[_ranX + i][_ranY] !== ' ') return false;
+        }
+        else {
+            if(_gridArray[_ranX][_ranY - i] !== ' ') return false;
+        }
+    }
 
     return true;
 }
@@ -190,9 +208,9 @@ function placeBoats() {
     const boatColours = ["#c91847", "#4ea9d0", "limegreen", "#f6ae2d", "#ef2ac9"];
 
     for (let i = 0; i < boatLengths.length; i++) {
-        let canPlace = placeRandomBoat(boatLengths[i], boatColours[i]);
+        let canPlace = placeRandomBoat(boatLengths[i], i + 1, boatColours[i]);
         while(!canPlace) {
-            canPlace = placeRandomBoat(boatLengths[i], boatColours[i]);
+            canPlace = placeRandomBoat(boatLengths[i], i + 1, boatColours[i]);
         }
     }
 }
