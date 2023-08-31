deb5481742914a4d2bc2cacc4fdb4cd8ba35dd0a
JoelLuckett <joelluckett@icloud.com>
2023-08-31 20:16:06 +0100

    modified games icon container css

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -2,3 +2,8 @@ function toggleMenu(x)
 {
     x.classList.toggle("change");
 }
+
+$(document).ready(function() {
+    console.log($(".topnav").outerHeight(true));
+    console.log(window.screen.height);
+});
